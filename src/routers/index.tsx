9b7ff7e182c29445417767fae95d7b11e70f4aa9
tranvansy <sytranvan220501@gmail.com>
2022-10-16 23:20:19 +0700

    ad gd login and register

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
 import RenderRouteHeader from './RenderRouteHeader'
+import LoginPage from '../pages/auth/login/LoginPage'
+import RegisterPage from '../pages/auth/register/RegisterPage'
+
 
 const mainRoutes = [
 	{
@@ -46,8 +49,8 @@ const index = () => {
 					<Route path="*" element={<>Error page</>} />
 				</Route>
 
-				<Route path="/login" element={<>Login page</>} />
-				<Route path="/register" element={<>register page</>} />
+				<Route path="/login" element={<LoginPage/>} />
+				<Route path="/register" element={<RegisterPage/>} />
 			</Routes>
 		</div>
 	)
