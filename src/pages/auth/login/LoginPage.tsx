cbb434ea13ae046f7a98b5f7629dac77f98146be
tranvansy <sytranvan220501@gmail.com>
2022-10-18 11:19:58 +0700

    commit

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -1,36 +1,39 @@
-import React from "react";
-import { Col, Row, Image, Form, Button } from "antd";
-import FormLogin from "../login/components/FormLogin"
-import styles from "./LoginPage.module.scss"
-import ImageLogo from "../../../assets/image/imageLogo.png"
+import React from 'react'
+import { Col, Row, Image, Form, Button } from 'antd'
+import FormLogin from '../login/components/FormLogin'
+import styles from './LoginPage.module.scss'
+import ImageLogo from '../../../assets/image/imageLogo.png'
+import { Link } from 'react-router-dom'
 
 const LoginForm = () => {
-  return (
-    <div className={styles.container}>
-        <Row justify="center">
-          <Col span={10}>
-            <Image src={ImageLogo} alt="image" preview={false}/>
-          </Col>
-          <Col span={7}>
-            <Row>
-              <FormLogin></FormLogin>
-            </Row>
-            <Row className="row-rec-register" align="middle">
-              <Col span={24}>
-                <Form wrapperCol={{ span: 20 }}  className={styles.container__redirectregister}>
-                  <Form.Item >
-                    <span>Bạn quên mật khẩu ?</span>
-                    <Button type="link" size="middle">
-                      Đăng ký
-                    </Button>
-                  </Form.Item>
-                </Form>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-    </div>
-  );
-};
+	return (
+		<div className={styles.container}>
+			<Row justify="center">
+				<Col span={12}>
+					<div style={{ height: '100%' }}>
+            <img src={ImageLogo} alt="image" style={{width:"100%",height:"100%"}}/>
+					</div>
+				</Col>
+				<Col span={10}>
+					<div style={{ width: '100%F' }}>
+						<Row justify="center">
+							<FormLogin></FormLogin>
+						</Row>
+						<Row className="row-rec-register" align="middle">
+							<Col span={24}>
+								<Form wrapperCol={{ span: 20 }} className={styles.container__redirectregister}>
+									<Form.Item>
+										<span>Bạn quên mật khẩu ?</span>
+										<Link to="/register">Đăng kí</Link>
+									</Form.Item>
+								</Form>
+							</Col>
+						</Row>
+					</div>
+				</Col>
+			</Row>
+		</div>
+	)
+}
 
-export default LoginForm;
+export default LoginForm
--- src/pages/auth/login/components/FormLogin.tsx
+++ src/pages/auth/login/components/FormLogin.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Button, Form, Input, Image, Divider } from 'antd'
+import { Button, Form, Input, Image, Divider, Row, Col } from 'antd'
 import { FacebookFilled } from '@ant-design/icons'
 import styles from '../LoginPage.module.scss'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 function Login() {
+	const { t } = useTranslation();
+
 	const onFinish = () => {
 		console.log('Success:')
 	}
@@ -12,57 +16,64 @@ function Login() {
 		console.log('Failed:')
 	}
 	return (
-		<Form
-			className={styles.container__antform}
-			labelCol={{ flex: '110px' }}
-			labelAlign="left"
-			labelWrap
-			wrapperCol={{ span: 20 }}
-			initialValues={{ remember: true }}
-			onFinish={onFinish}
-			onFinishFailed={onFinishFailed}
-			autoComplete="off"
-		>
-			<Form.Item>
-				<Image
-					src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F04%2FInstagram-Logo-2010-2013.png&f=1&nofb=1&ipt=b62dc7579e3af6fffec776ec72377be923cd58b6aa4670d7aff9ecb6976faa0f&ipo=images"
-					alt="logo"
-				/>
-			</Form.Item>
-			<Form.Item name="username">
-				<Input size="middle" placeholder="phone number, username or email" />
-			</Form.Item>
-
-			<Form.Item name="password">
-				<Input.Password size="middle" placeholder="password" />
-			</Form.Item>
-			<Form.Item>
-				<Button
-					className={styles.Container}
-					type="primary"
-					htmlType="submit"
-					size="middle"
-					id="btn-submit"
+		<Row justify="center" className={styles.container__border}>
+			<Col span={20}>
+				<Form
+					className={styles.container__antform}
+					wrapperCol={{ span: 24 }}
+					initialValues={{ remember: true }}
+					onFinish={onFinish}
+					onFinishFailed={onFinishFailed}
+					autoComplete="off"
 				>
-					Login
-				</Button>
-			</Form.Item>
-			<Form.Item>
-				<Divider className={styles.container__antform__divider} orientation="center">
-					OR
-				</Divider>
-			</Form.Item>
-			<Form.Item>
-				<Button type="link" icon={<FacebookFilled />} size="large">
-					Đăng nhập với Facebook
-				</Button>
-			</Form.Item>
-			<Form.Item>
-				<Button style={{ color: 'black', width: '100%' }} type="link">
-					Quên mật khẩu?
-				</Button>
-			</Form.Item>
-		</Form>
+					<Link to="/">
+						<Image
+							src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F04%2FInstagram-Logo-2010-2013.png&f=1&nofb=1&ipt=b62dc7579e3af6fffec776ec72377be923cd58b6aa4670d7aff9ecb6976faa0f&ipo=images"
+							alt="logo"
+							preview={false}
+						/>
+					</Link>
+					<Form.Item name="username" style={{ marginBottom: '10px' }}>
+						<Input size="large" placeholder="phone number, username or email" />
+					</Form.Item>
+
+					<Form.Item name="password" style={{ marginBottom: '16px' }}>
+						<Input.Password size="large" placeholder="password" />
+					</Form.Item>
+					<Form.Item style={{ marginBottom: '10px' }}>
+						<Button
+							className={styles.Container}
+							type="primary"
+							htmlType="submit"
+							size="large"
+							id="btn-submit"
+						>
+							Login
+						</Button>
+					</Form.Item>
+					<Form.Item style={{ marginBottom: '0px' }}>
+						<Divider className={styles.container__antform__divider} orientation="center">
+							OR
+						</Divider>
+					</Form.Item>
+					<Form.Item style={{ marginBottom: '16px' }}>
+						<Button
+							type="link"
+							icon={<FacebookFilled />}
+							size="large"
+							className={styles.container__antform__btnloginwfb}
+						>
+							Đăng nhập với Facebook
+						</Button>
+					</Form.Item>
+					<Form.Item>
+						<Button style={{ color: 'black', width: '100%' }} type="link">
+							Quên mật khẩu?
+						</Button>
+					</Form.Item>
+				</Form>
+			</Col>
+		</Row>
 	)
 }
 
