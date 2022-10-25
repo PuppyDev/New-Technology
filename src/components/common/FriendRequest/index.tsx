4caeeb8c58cc9726ba5bf1451fd859a65b4adf2d
tranvansy <sytranvan220501@gmail.com>
2022-10-25 13:42:30 +0700

    add dropdown check friends request

---
+++ src/components/common/FriendRequest/index.tsx
@@ -0,0 +1,51 @@
+import { UserOutlined } from '@ant-design/icons'
+import { Avatar, Button, Col, Row } from 'antd'
+import React from 'react'
+import styles from './FriendRequest.module.scss'
+import { useTranslation } from 'react-i18next'
+
+// import { Container } from './styles';
+
+interface props {
+	avatar: any
+	username: any
+	time: any
+}
+
+const FriendRequest: React.FC<props> = ({ avatar, username, time }) => {
+	const { t } = useTranslation()
+
+	const handleClickCofirm = () => {}
+	const handleClickDelete = () => {}
+
+	return (
+		<div>
+			<span style={{ fontSize: '12px' }}>{t('FRIEND_REQUEST')}</span>
+			<Row>
+				<Col span={3}>
+					<Avatar size="default" icon={<UserOutlined />} src={avatar} />
+				</Col>
+				<Col span={21}>
+					<Row style={{ paddingTop: '3px' }}>
+						<p style={{ fontWeight: 500, paddingRight: '5px' }}>{username}</p>
+
+						<p>{t('FRIEND_SEND_REQUEST')}</p>
+					</Row>
+					<Row>
+						<p style={{ fontSize: '11px' }}>{time}</p>
+					</Row>
+				</Col>
+			</Row>
+			<Row justify="end" style={{ paddingBottom: '5px' }}>
+				<Button type="primary" size="small" style={{ marginRight: '2px' }} onClick={handleClickCofirm}>
+					{t('CONFIRM')}
+				</Button>
+				<Button size="small" onClick={handleClickDelete}>
+					{t('DELETE')}
+				</Button>
+			</Row>
+		</div>
+	)
+}
+
+export default FriendRequest
--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -10,7 +10,7 @@ import {
 	SwapOutlined,
 	UserOutlined,
 } from '@ant-design/icons'
-import { Avatar, Dropdown, Menu, Modal, Space } from 'antd'
+import { Avatar, Dropdown, Form, Menu, Modal, Space } from 'antd'
 import { logout } from 'pages/auth/authSlice'
 import React, { useState } from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
@@ -18,16 +18,14 @@ import ModalLogin from '../Modal/ModalLogin'
 import ModalPost from '../Modal/ModalPost'
 import styles from './Header.module.scss'
 import ImageLogo from '../../../assets/image/bbsgl.png'
+import FriendRequest from '../FriendRequest'
 
 const Header = () => {
 	return (
 		<div className={styles.header}>
 			<div className={styles.header__container}>
 				<Link to="/" className={styles.header__containerlogo}>
-					<img
-						src={ImageLogo}
-						alt="logo"
-					/>
+					<img src={ImageLogo} alt="logo" />
 				</Link>
 
 				<nav className={styles.header__containernav}>
@@ -63,6 +61,63 @@ const ControlNavLink: React.FC = () => {
 			centered: true,
 		})
 	}
+	const menuNotification = (
+		<div>
+			<Menu
+				className={styles.menuNotification}
+				items={[
+					{
+						key: '1',
+						label: (
+							<FriendRequest
+								avatar={
+									'https://scontent.fsgn3-1.fna.fbcdn.net/v/t39.30808-1/312603886_1521295268335191_853326481758830544_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=lgwIhizXZjEAX8bRm6B&_nc_ht=scontent.fsgn3-1.fna&oh=00_AT_L5QZnnba1SzoSNZKOIN2gJSTuKZsLg5HHGZvbEX_x_Q&oe=635CE66C'
+								}
+								username={'Trần Sỹ'}
+								time={'1 ngày trước'}
+							/>
+						),
+					},
+					{
+						key: '2',
+						label: (
+							<FriendRequest
+								avatar={
+									'https://scontent.fsgn4-1.fna.fbcdn.net/v/t1.6435-1/150824650_907704233133970_7361803211201239538_n.jpg?stp=dst-jpg_p200x200&_nc_cat=103&ccb=1-7&_nc_sid=7206a8&_nc_ohc=OYV4avReFY0AX-Rc0Ag&_nc_ht=scontent.fsgn4-1.fna&oh=00_AT9_52vObnUMKV9H5r7HuG6_ST_axAOuXwbADw6_fvtQZg&oe=637D6B30'
+								}
+								username={'Bảo Đoàn'}
+								time={'3 ngày trước'}
+							/>
+						),
+					},
+					{
+						key: '3',
+						label: (
+							<FriendRequest
+								avatar={
+									'https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-1/311743184_1819918978349276_514877341982511448_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=7206a8&_nc_ohc=Lv7vfAK6XicAX8l4erc&_nc_ht=scontent.fsgn13-2.fna&oh=00_AT_3GyvPkHWl7XF7CweHNgF39-z0fHjzQIEQKFKHjn7NHw&oe=635C431E'
+								}
+								username={'Bảo Huỳnh'}
+								time={'1 tuần trước'}
+							/>
+						),
+					},
+					{
+						key: '4',
+						label: (
+							<FriendRequest
+								avatar={
+									'https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-1/305220249_3390900867859963_8999404357755860763_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=7206a8&_nc_ohc=b_E8Ag7h3fcAX_n84ai&_nc_ht=scontent.fsgn4-1.fna&oh=00_AT-ZPzRsE3MzsgL6Dkmo39MdkLjpg4C9sJIzT5IuRgnF4w&oe=635C675B'
+								}
+								username={'Giang Võ'}
+								time={'1 tuần trước'}
+							/>
+						),
+					},
+				]}
+			/>
+		</div>
+	)
 
 	const menu = (
 		<Menu
@@ -146,9 +201,16 @@ const ControlNavLink: React.FC = () => {
 				</NavLink>
 			</li>
 			<li>
-				<NavLink to="/211" className={({ isActive }) => (isActive ? styles.active : '')}>
-					<HeartOutlined />
-				</NavLink>
+				<Dropdown
+					overlay={menuNotification}
+					placement="bottomRight"
+					arrow={{ pointAtCenter: true }}
+					trigger={['click']}
+				>
+					<Space>
+						<HeartOutlined />
+					</Space>
+				</Dropdown>
 			</li>
 			<li>
 				<Dropdown overlay={menu} placement="bottomRight" arrow={{ pointAtCenter: true }} trigger={['click']}>
