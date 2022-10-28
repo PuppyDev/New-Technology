4448b1b890256af5adba7cc8bedcc11d3056b0b8
tranvansy <sytranvan220501@gmail.com>
2022-10-28 23:03:05 +0700

    update components kết bạn

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -10,7 +10,7 @@ import {
 	SwapOutlined,
 	UserOutlined,
 } from '@ant-design/icons'
-import { Avatar, Dropdown, Form, Menu, Modal, Space } from 'antd'
+import { Avatar, Badge, Dropdown, Form, Menu, Modal, Space } from 'antd'
 import { logout } from 'pages/auth/authSlice'
 import React, { useState } from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
@@ -63,59 +63,55 @@ const ControlNavLink: React.FC = () => {
 	}
 	const menuNotification = (
 		<div>
-			<Menu
-				className={styles.menuNotification}
-				items={[
-					{
-						key: '1',
-						label: (
-							<FriendRequest
-								avatar={
-									'https://scontent.fsgn3-1.fna.fbcdn.net/v/t39.30808-1/312603886_1521295268335191_853326481758830544_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=lgwIhizXZjEAX8bRm6B&_nc_ht=scontent.fsgn3-1.fna&oh=00_AT_L5QZnnba1SzoSNZKOIN2gJSTuKZsLg5HHGZvbEX_x_Q&oe=635CE66C'
-								}
-								username={'Trần Sỹ'}
-								time={'1 ngày trước'}
-							/>
-						),
-					},
-					{
-						key: '2',
-						label: (
-							<FriendRequest
-								avatar={
-									'https://scontent.fsgn4-1.fna.fbcdn.net/v/t1.6435-1/150824650_907704233133970_7361803211201239538_n.jpg?stp=dst-jpg_p200x200&_nc_cat=103&ccb=1-7&_nc_sid=7206a8&_nc_ohc=OYV4avReFY0AX-Rc0Ag&_nc_ht=scontent.fsgn4-1.fna&oh=00_AT9_52vObnUMKV9H5r7HuG6_ST_axAOuXwbADw6_fvtQZg&oe=637D6B30'
-								}
-								username={'Bảo Đoàn'}
-								time={'3 ngày trước'}
-							/>
-						),
-					},
-					{
-						key: '3',
-						label: (
-							<FriendRequest
-								avatar={
-									'https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-1/311743184_1819918978349276_514877341982511448_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=7206a8&_nc_ohc=Lv7vfAK6XicAX8l4erc&_nc_ht=scontent.fsgn13-2.fna&oh=00_AT_3GyvPkHWl7XF7CweHNgF39-z0fHjzQIEQKFKHjn7NHw&oe=635C431E'
-								}
-								username={'Bảo Huỳnh'}
-								time={'1 tuần trước'}
-							/>
-						),
-					},
-					{
-						key: '4',
-						label: (
-							<FriendRequest
-								avatar={
-									'https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-1/305220249_3390900867859963_8999404357755860763_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=7206a8&_nc_ohc=b_E8Ag7h3fcAX_n84ai&_nc_ht=scontent.fsgn4-1.fna&oh=00_AT-ZPzRsE3MzsgL6Dkmo39MdkLjpg4C9sJIzT5IuRgnF4w&oe=635C675B'
-								}
-								username={'Giang Võ'}
-								time={'1 tuần trước'}
-							/>
-						),
-					},
-				]}
-			/>
+			<Menu className={styles.menuNotification}>
+				<Menu.ItemGroup title={'Friends request'}>
+					<Menu.Item key={1}>
+						<FriendRequest
+							avatar={
+								'https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/312726788_1729002340788975_614199594052393060_n.jpg?stp=dst-jpg_p320x320&_nc_cat=111&ccb=1-7&_nc_sid=7206a8&_nc_ohc=WXY_hfL1NpAAX-T6erh&_nc_ht=scontent.fsgn2-6.fna&oh=00_AfBWBomSY2HzNaH3YY4FsshIwiJB_hrGPXRoY5ZltfIRRA&oe=63603AE8'
+							}
+							username={'Trần Sỹ'}
+							time={'1 tuần trước'}
+						/>
+					</Menu.Item>
+					<Menu.Item key={2}>
+						<FriendRequest
+							avatar={
+								'https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/312726788_1729002340788975_614199594052393060_n.jpg?stp=dst-jpg_p320x320&_nc_cat=111&ccb=1-7&_nc_sid=7206a8&_nc_ohc=WXY_hfL1NpAAX-T6erh&_nc_ht=scontent.fsgn2-6.fna&oh=00_AfBWBomSY2HzNaH3YY4FsshIwiJB_hrGPXRoY5ZltfIRRA&oe=63603AE8'
+							}
+							username={'Trần Sỹ'}
+							time={'1 tuần trước'}
+						/>
+					</Menu.Item>
+					<Menu.Item key={3}>
+						<FriendRequest
+							avatar={
+								'https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/312726788_1729002340788975_614199594052393060_n.jpg?stp=dst-jpg_p320x320&_nc_cat=111&ccb=1-7&_nc_sid=7206a8&_nc_ohc=WXY_hfL1NpAAX-T6erh&_nc_ht=scontent.fsgn2-6.fna&oh=00_AfBWBomSY2HzNaH3YY4FsshIwiJB_hrGPXRoY5ZltfIRRA&oe=63603AE8'
+							}
+							username={'Trần Sỹ'}
+							time={'1 tuần trước'}
+						/>
+					</Menu.Item>
+					<Menu.Item key={4}>
+						<FriendRequest
+							avatar={
+								'https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/312726788_1729002340788975_614199594052393060_n.jpg?stp=dst-jpg_p320x320&_nc_cat=111&ccb=1-7&_nc_sid=7206a8&_nc_ohc=WXY_hfL1NpAAX-T6erh&_nc_ht=scontent.fsgn2-6.fna&oh=00_AfBWBomSY2HzNaH3YY4FsshIwiJB_hrGPXRoY5ZltfIRRA&oe=63603AE8'
+							}
+							username={'Trần Sỹ'}
+							time={'1 tuần trước'}
+						/>
+					</Menu.Item>
+					<Menu.Item key={5}>
+						<FriendRequest
+							avatar={
+								'https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-1/312726788_1729002340788975_614199594052393060_n.jpg?stp=dst-jpg_p320x320&_nc_cat=111&ccb=1-7&_nc_sid=7206a8&_nc_ohc=WXY_hfL1NpAAX-T6erh&_nc_ht=scontent.fsgn2-6.fna&oh=00_AfBWBomSY2HzNaH3YY4FsshIwiJB_hrGPXRoY5ZltfIRRA&oe=63603AE8'
+							}
+							username={'Trần Sỹ'}
+							time={'1 tuần trước'}
+						/>
+					</Menu.Item>
+				</Menu.ItemGroup>
+			</Menu>
 		</div>
 	)
 
@@ -208,12 +204,14 @@ const ControlNavLink: React.FC = () => {
 					trigger={['click']}
 				>
 					<Space>
-						<HeartOutlined />
+						<Badge count={5} size={'small'} style={{}}>
+							<HeartOutlined />
+						</Badge>
 					</Space>
 				</Dropdown>
 			</li>
 			<li>
-				<Dropdown overlay={menu} placement="bottomRight" arrow={{ pointAtCenter: true }} trigger={['click']}>
+				<Dropdown overlay={menu} placement="bottomRight" arrow={{ pointAtCenter: true }} trigger={['click']} >
 					<Space>
 						<Avatar icon={<UserOutlined />} size={26}></Avatar>
 					</Space>
